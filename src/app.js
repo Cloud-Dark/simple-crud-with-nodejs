ea41d3a300000b73056c23b3950ecd67f7a3e432
Cloud Dark <clouddark1798@gmail.com>
2023-02-17 18:51:52 +0700

    add simple debugging with pino

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const list_taskRoute = require("./routes/list_task.route")
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+
+const expressLogger = expressPino({ logger });
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(expressLogger);
+
+
 
 // import model
 const database = require("./database")
@@ -13,9 +23,11 @@ database.sync({ force: false , alter : true }).then(() => {
     console.info("database synced")
 }).catch(err => {
     console.error("failed to sync database: " + err.message)
+    process.exit(1)
 })
 
 app.get("/", (req, res) => {
+    logger.debug('Calling res.send');
     res.json({
         message: "Node.js Simple REST API Server"
     })
@@ -25,4 +37,6 @@ app.get("/", (req, res) => {
 // tambahkan list_task route ke dalam aplikasi
 app.use("/api/list_tasks", list_taskRoute)
 
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+app.listen(port, () => {
+    logger.info('Server running on port %d', port);
+});
