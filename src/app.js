6b68ff20415ce45650d468d1db540159274c4861
Cloud Dark <clouddark1798@gmail.com>
2023-02-17 16:24:41 +0700

    add config db

--- src/app.js
+++ src/app.js
@@ -6,10 +6,21 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// import model
+const database = require("./database")
+
+database.sync({ force: true }).then(() => {
+    console.info("database synced")
+}).catch(err => {
+    console.error("failed to sync database: " + err.message)
+})
+
 app.get("/", (req, res) => {
     res.json({
         message: "Node.js Simple REST API Server"
     })
 })
+
+
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
