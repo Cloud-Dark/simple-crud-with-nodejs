8b8f9c3e36370508946a44687d457c2e8b77b1eb
Cloud Dark <clouddark1798@gmail.com>
2023-02-18 10:25:45 +0700

    Update validation.middleware.js
    bug fix logger debug

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -17,12 +17,9 @@ exports.createBookValidation = (req, res, next) => {
 	const { judul, selesai } = req.body;
 
 	if (judul === undefined || judul == "") {
-        logger.info("Judul Wajib diisi");
 		return response(res, 400, false, "Judul Wajib Di isi");
 	}
 	if (checkBool(selesai) == false) {
-        logger.debug(checkBool(selesai));
-        logger.info("'selesai' hanya boleh menampung nilai true dan false");
 		return response(res, 501, false, "'selesai' boleh menampung nilai true dan false");
 	}
 
